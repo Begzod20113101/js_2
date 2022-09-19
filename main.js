edd12b4353c54fb4b58d0ccc42797fb4611dc8bf
Begzod20113101 <akbaevanilufar@gmail.com>
2022-09-19 14:39:28 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,323 @@
+// const arr = [1,2,3,4]
+// console.log(arr);
+
+// for(let i = inital; length; i++ ){
+//     console.log(arr);
+// }
+
+
+// const arr = [
+//     {
+//         name: "Begzod",
+//         age: 11,
+//         merrit: false,
+
+
+//     }
+// ]
+// console.log(arr);
+
+
+// const arr = [
+//     {
+//         name: "Begzod",
+//         score:70,
+//         asserts:"toshkent",
+//     }, 
+//     {
+//         name: "Begzod",
+//         score:70,
+//         asserts:"toshkent",
+//     },
+//     {
+//         name: "Begzod",
+//         score:70,
+//         asserts:"toshkent",
+//     },{
+//         name: "Begzod",
+//         score:10,
+//         asserts:"toshkent",
+//     }, {
+//         name: "Begzod",
+//         score:10,
+//         asserts:"toshkent",
+//     },  {
+//         name: "Begzod",
+//         score:10,
+//         asserts:"toshkent",
+//     }
+// ]
+
+
+// function filterstudent(student) {
+//     for (let i = 0; i < student.length; i++){
+//         if (student[i].score>20) {
+//             console.log(
+//                 student[i].name +""+"imtihondan otingiz"
+//             );
+
+//         } else{
+//             console.log(
+//                student[i].name + "" + "qayta oqing"
+//             );
+//         }
+//     }
+// }
+
+// filterstudent(arr)
+
+
+// const inputVales = prompt("")
+
+// let alcref = 500;
+// let holet =250;
+// let mazem =120;
+// let usd = 1100;
+// let eu = 11200;
+// const exispedutere = (alcref + holet) * usd  / mazem + eu
+
+
+// if(inputVales > exispedutere )  {
+//     console.log("yol olishga tayorlaning");
+// } else {
+//     console.log("yan biroz sabir qiling");
+// }
+
+
+// const nums = [2,7,6,8,7];
+
+// const target = 9;
+
+// function addtarget (nums , target){
+//     let indexes = [];
+//     for (let j = i+1 ; j < nums.length; i++){
+//         if (nums[i] + nums[j] === target){
+//             indexes.push(nums[i])
+//             indexes.push(nums[i])     
+//         }
+//     }
+// }
+
+
+// let i = 1, n = 5;
+
+// while (i<= 5) {
+//     console.log(i);
+//     i += 1
+// }
+
+// function main(a){
+//     let newArr = []
+//     for(let i = 1; i<= a/2;i++){
+// if
+//     }
+// }
+
+// for(letm i = 0; i<2;(i*10+2)/10){
+// console.log(i);
+// }
+
+// let n=10;
+// let k=2;
+
+
+// for(let i =0;i<n;i++){
+//     console.log(i+'|'+k)
+// }
+
+// let str = 'dunyo'
+
+// for (let i =0 ;i < str.length ; i++){
+//     console.log(str[i]);
+// }
+
+// function main(str){
+
+// }
+
+
+
+
+// let a = 10;
+
+// for ( let i =0 ; i <10 ; i++ ){
+//     console.log(i);
+// }
+
+
+
+// let k = 10;
+// let n = 10;
+
+// let kopayytru = (k * n )
+
+// for (let i=0 ; i< kopayytru.length ; i++ ){
+//     console.log(kopayytru);
+// }
+
+// let a = 19;
+// let b =20;
+
+// for (let i = 0 ; i<= a ; i++){
+//     console.log(i);
+// }
+
+
+
+// let kg = 1000;
+// let kg2 = 20000;
+// let kg3 = 30000;
+// let kg4 = 40000;
+// let kg5 = 50000;
+// let kg6 = 60000;
+// let kg7 = 70000;
+// let kg8 = 80000;
+// let kg9 = 90000;
+
+
+// if   (kg){
+//     console.log("1 kg 10000 so`m ");
+// }
+
+// if (kg2) {
+//     console.log("2 kg 20000 so`m");
+// } if  (kg3){
+//     console.log("3 kg 30000 so`m ");
+// }
+//  if  (kg4){
+//     console.log("4 kg 40000 so`m ");
+// }
+// if  (kg5){
+//     console.log("5 kg 50000 so`m ");
+// }
+// if  (kg6){
+//     console.log("6 kg 60000 so`m ");
+// }
+// if  (kg7){
+//     console.log("7 kg 70000 so`m ");
+// }
+// if  (kg8){
+//     console.log("3 kg 80000 so`m ");
+// }
+// if  (kg9){
+//     console.log("9 kg 90000 so`m ");
+// }
+
+
+// let a = 10;
+// let b = 10;
+
+// let yigindi = 2 + 4 + 6 + 8 + 10 ;
+
+// if (yigindi){
+//     console.log(yigindi);
+// }
+
+
+// let a = 10;
+// let b = 10;
+// let kopaytma  = 2 * 4 * 6 * 8 * 10 ;
+
+// if (kopaytma){
+//     console.log(kopaytma);
+// }
+
+
+// let t1 = 10;
+// let t2 = 20;
+// let t3 = 30;
+// let t4 = 40;
+// let t5 = 10;
+// let t6 = 10;
+// let t7 = 10;
+// let t8 = 10;
+// let t9 = 10;
+// let t10 = 70;
+// let t11 = 10;
+// let t12 = 10;
+
+// let yigindi = 10 + 20 + 30 + 40 + 10 + 10 + 10 + 10 + 10 + 70 + 10 +10;
+
+
+// if ( yigindi > 0 ){
+//     console.log(yigindi);
+// }
+
+
+// let k=10;
+// let n = 40;
+
+// for (let i = 0; i< n.length; i++){
+//     console.log(i);
+// }
+
+
+
+
+// function main (a,b){
+//     if (a-b == 2){
+//         return a-b 
+//     } else {
+//         return a + b
+//     }
+// }
+
+// let sum = main;
+
+
+// console.log(sum (2,3));
+
+
+// let n = 10 ;
+// let k = 10;
+
+// let yigindi = n + k;
+
+
+// for  (let i = 0; i<yigindi; i++ ){
+//     console.log(i);
+//     if {
+
+//     }
+// }
+
+
+// for (let son = 1; son <= 100; son=son+1){
+   
+//     bu_tup_son= true;
+//     for (let but = 2; bul < son ; bul = bul + 1){
+//         if(son % bul == 0){
+//             bu_tup_son=false;
+//             break
+//         }
+//     }
+//     if (bu_tup_son== true){
+//         console.log(son);
+//     }
+// }
+
+// const main = (a) => {
+//     for (let i = 1; i <= a; i++){
+//         if (i%2 === 0){
+//             console.log(i + '_____ juft son');
+//         }else {
+//             console.log(i + '_____ toq son');
+//         }
+//     }
+// }
+// console.log(main(100));
+
+// function time (a){
+//     let min = (a%2 === 0)   ? 0 : a/60
+//     if(
+//         a%2 === 0) {
+//             min = 0
+//     } else {
+//         min =a/60
+//     }
+//     let time = Math.floor(a/3600)
+
+//     return " ${time} soat ${min} minut o`tdi"
+// }
+
+// console.log(time(3600));
